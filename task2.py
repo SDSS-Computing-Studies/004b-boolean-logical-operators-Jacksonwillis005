bda0b9e834d06f90e5bcdef112b74e4a063ea9e4
WillisJackson <jacksonw005@deltalearns.ca>
2021-02-16 09:45:57 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -31,10 +31,10 @@ Enter a number: 8
 import math
 
 xx=input("Enter a Number: ")
+xx=float(xx)
 if xx**(1/3):
     print("xx is only a perfect cube.")
 if xx**(1/2):
     print("xx is only a perfect square.")
-if xx**(1/3)
-    if xx**(1/2)
+if xx**(1/3) and xx**(1/2):
         print("xx is both a perfect square and a perfect cube.")
